5487d5107ef9cdc59778e8567799087f6135f7b7
guokairui <guokairui@caiji668.com>
2023-10-17 14:55:42 +0800

    feat: 完成开发

---
+++ README.md
@@ -0,0 +1,3 @@
+# dy-i18n README
+
+显示 lang.yaml 中对应的语言，支持跳转
---
+++ src/test/index.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+const langYamlContent = fs.readFileSync(
+  "d:\\code\\other\\test-pb2api\\chikii\\test\\lang.yaml",
+  "utf-8"
+);
+console.log("🚀 ~ file: index.js:5 ~ langYamlContent:", langYamlContent);
